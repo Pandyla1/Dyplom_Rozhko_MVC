a12fc4a4a96205953b060283595e7c6638030413
Alosha <micronblogger@gmail.com>
2025-03-31 12:02:27 +0300

    Add project files.

---
+++ Dyplom_Rozhko_MVC/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dyplom_Rozhko_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
