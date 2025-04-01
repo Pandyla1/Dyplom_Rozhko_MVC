ce7763932d8e58c2300d69e54798efb137598717
Alosha <micronblogger@gmail.com>
2025-04-01 17:54:41 +0300

    Added working registration without email validation and checking for existing mail/phone

--- Dyplom_Rozhko_MVC/Controllers/AccountController.cs
+++ Dyplom_Rozhko_MVC/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dyplom_Rozhko_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +14,34 @@ namespace Dyplom_Rozhko_MVC.Controllers
             return View();
         }
 
-        public ActionResult RegisterPage()
+        [HttpGet]
+        public ActionResult Register()
         {
-            return View();
+            DyplomEntities db = new DyplomEntities();
+            var viewModel = new ConnectAllTables
+            {
+                Category = db.Category.ToList(),
+                Users = db.Users.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(Users users, string NameUser, string RealNameUser, string PhoneNumber, string Email, string Password)
+        {
+            DyplomEntities db = new DyplomEntities();
+
+            users.Name = RealNameUser;
+            users.UserName = NameUser;
+            users.Phone= PhoneNumber;
+            users.Email = Email;
+            users.Password = Password;
+            users.RoleId = 1;
+            users.CreatedDate = DateTime.Now;
+            
+            db.Users.Add(users);
+            db.SaveChanges();
+            return RedirectToAction("Index", "User");
         }
     }
 }
